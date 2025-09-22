4a6b26675513073ec86edfcde9b9cba708523fb9
Akash Baghel <akash27aug.2002@gmail.com>
2025-09-22 08:08:19 +0530

    Hero complete

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,6 +11,33 @@ const Hero = () => {
       <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nisi cum, nemo porro iusto soluta odio quia quidem dolor inventore. Consectetur unde adipisci ipsum distinctio.
       </p>
+      <div className='flex justify-center m-10'>
+        <a href="#" 
+        className='bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-2 rounded-md'>
+            Start for free
+        </a>
+        <a href="#" className='py-3 px-4 mx-2 rounded-md border'>
+            Documentation
+        </a>
+      </div>
+      <div className='flex mt-10 justify-center'>
+        <video 
+        src={video1}
+        loop 
+        muted 
+        autoPlay
+        className='rounded-lg w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4'
+        >
+        </video>
+        <video 
+        src={video2}
+        loop 
+        muted 
+        autoPlay
+        className='rounded-lg w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4'
+        >
+        </video>
+      </div>
     </div>
   )
 }
