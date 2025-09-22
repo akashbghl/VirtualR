7ac22de5e9d0f3f34f4b01a3ac58378c1e3a71e7
Akash Baghel <akash27aug.2002@gmail.com>
2025-09-22 10:07:56 +0530

    imports update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Feature from "./components/Feature"
 import Footer from "./components/Footer"
-import Hero from "./components/hero"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Pricing from "./components/Pricing"
 import Testimonials from "./components/Testimonials"
