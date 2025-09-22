5d12f9fd64c6b40c29beb86e33bcc00098d3e859
Akash Baghel <akash27aug.2002@gmail.com>
2025-09-22 09:08:43 +0530

    workflowSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Feature from "./components/Feature"
 import Hero from "./components/hero"
 import Navbar from "./components/Navbar"
+import Workflow from "./components/Workflow"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero/>
         <Feature/>
+        <Workflow/>
       </div>
     </div>
   )
